4044a09f2e2852f44d76bc227c61c4699e60c27d
“sandip5” <singhsandeep.singh97@gmail.com>
2020-07-13 21:38:50 +0530

    ADDED- Test Case For Give 0 Feet And 0 Feet Should Return Equal

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -1,12 +1,21 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+
 public class QuantityMeasurementTest {
+
     @Test
     public void givenQuantityInFeet_ShouldReturnQuantityInInches() {
         QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
         double numberInFeet = 5.0;
         double convertedQuantity = quantityMeasurement.unitConverter(numberInFeet);
         Assert.assertEquals(60.0,convertedQuantity,0.0);
+    }
+
+    @Test
+    public void givenZeroFeetAndZeroFeet_ShouldReturnEqual() {
+        Feet firstFeet = new Feet(0.0);
+        Feet secondFeet = new Feet(0.0);
+        Assert.assertEquals(firstFeet,secondFeet);
     }
 }
