2ea52b38f23d1eb95140c05850b95b00a80e94ec
“sandip5” <singhsandeep.singh97@gmail.com>
2020-07-15 00:19:49 +0530

    ADDED- Enum Type Conversion Factor Added For Conversion Inch To CM

--- src/main/java/com/bridgelabz/utility/ConversionType.java
+++ src/main/java/com/bridgelabz/utility/ConversionType.java
@@ -2,7 +2,7 @@ package com.bridgelabz.utility;
 
 public enum ConversionType {
     FEET_TO_INCH(12.0),INCH_TO_YARD(1d/36d),YARD_TO_FEET(3),
-    YARD_TO_INCH(36),FEET_TO_YARD(1d/3d),INCH_TO_CM(0);
+    YARD_TO_INCH(36),FEET_TO_YARD(1d/3d),INCH_TO_CM(5d/2d);
     public double conversionFactor;
 
     ConversionType(double conversionFactor) {
